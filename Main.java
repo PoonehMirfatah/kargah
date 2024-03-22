102401da22032fe4b7487a9bc08a99f12c4422ab
PoonehMirfatah <154900069+PoonehMirfatah@users.noreply.github.com>
2024-03-22 16:59:03 +0330

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        Name[] arrayNames = new Name[5];
+        arrayNames[3]=new Name("Pooneh","Mirfatah");
+        System.out.println(arrayNames[3].getFirstName());
+    }
+}
+
+class Name {
+    private String firstName;
+    private String lastName;
+
+    Name(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+}
